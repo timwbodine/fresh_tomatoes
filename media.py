c98c68184ba6eb1b0e24f3c9cb416d36296ab75a
skrimbleshanks <timwbodine@gmail.com>
2017-12-13 19:37:08 -0500

    Fork from udacity repo, create Media/Movie classes

---
+++ media.py
@@ -0,0 +1,24 @@
+
+class Media():
+    """this class provides a way to access abstract human-formatted media objects and serves as the root class for more specific human-formatted media types"""
+    VALID_EXPERIENCE_VECTORS = ["AURAL","VISUAL","OLFACTORY", "GUSTATORY" "TACTILE"]
+    def __init__(self, title, description, representative_image, experience_vectors):
+        self.title = title
+        self.description = description
+        self.representative_image = representative_image
+        self.experience_vectors = []
+        for experience_vector in experience_vectors:
+            if experience_vector in Media.VALID_EXPERIENCE_VECTORS:
+                self.experience_vectors.append(experience_vector)
+            else:
+                self.experience_vectors.append = "INVALID EXPERIENCE VECTOR '" + str(experience_vector) + "' INPUT AT THIS INDEX.  PLEASE CHECK COMPATIBILITY OF VECTOR WITH CURRENT EXISTENTIAL MODALITY"
+
+class Movie(Media):
+    """this class provides a convenient way to access media objects commonly considered to be movies"""
+    def __init__(self, title, description, representative_image, trailer, aggregate_rating):
+        Media.__init__(self, title, description, representative_image, ["AURAL", "VISUAL"])
+        self.trailer = trailer
+        self.aggregate_rating = aggregate_rating
+        
+class Show(Media):
+    """this class provides a convenient way to access media objects commonly considered to be shows"""
